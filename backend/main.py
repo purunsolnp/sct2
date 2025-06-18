64555550b3b319ef3b4072975502bab38d5ac281
purunsolnp <purunsolnp@gmail.com>
2025-06-18 12:45:16 +0900

    cors 수정

--- backend/main.py
+++ backend/main.py
@@ -175,7 +175,12 @@ app = FastAPI(
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500", "https://purunsolsct.netlify.app"],  # 프론트엔드 서버 주소
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://purunsolsct.netlify.app",
+        "https://psysct.netlify.app",  # 새 프론트엔드 도메인 추가
+    ],  # 프론트엔드 서버 주소
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
